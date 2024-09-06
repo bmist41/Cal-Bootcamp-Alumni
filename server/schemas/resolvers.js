fc4fb221226029ac42bb128157f9e1c949a4a7d8
ptprice <11355676+ptprice@users.noreply.github.com>
2024-09-05 22:11:05 -0600

    Update to the resolver file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,43 +1,105 @@
-const { Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Post, Comment } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    thoughts: async () => {
-      return Thought.find();
+    posts: async () => {
+      return await Post.find().populate('user').populate('comments');
     },
-
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
+    post: async (parent, { _id }) => {
+      return await Post.findById(_id).populate('user').populate({
+        path: 'comments',
+        populate: 'user'
+      });
+    },
+    user: async (parent, { _id }) => {
+      return await User.findById(_id);
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findById(context.user._id);
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
   },
 
   Mutation: {
-    addThought: async (parent, { thoughtText, thoughtAuthor }) => {
-      return Thought.create({ thoughtText, thoughtAuthor });
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
+
+      if (!user || !(await user.isCorrectPassword(password))) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
     },
-    addComment: async (parent, { thoughtId, commentText }) => {
-      return Thought.findOneAndUpdate(
-        { _id: thoughtId },
-        {
-          $addToSet: { comments: { commentText } },
-        },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
+
+    signup: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-    removeThought: async (parent, { thoughtId }) => {
-      return Thought.findOneAndDelete({ _id: thoughtId });
+
+    createPost: async (parent, { title, content }, context) => {
+      if (context.user) {
+        const post = await Post.create({
+          title,
+          content,
+          user: context.user._id
+        });
+        return post;
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
-    removeComment: async (parent, { thoughtId, commentId }) => {
-      return Thought.findOneAndUpdate(
-        { _id: thoughtId },
-        { $pull: { comments: { _id: commentId } } },
-        { new: true }
-      );
+
+    updatePost: async (parent, { _id, title, content }, context) => {
+      if (context.user) {
+        const post = await Post.findOneAndUpdate(
+          { _id, user: context.user._id },
+          { title, content },
+          { new: true }
+        );
+        return post;
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
-  },
+
+    deletePost: async (parent, { _id }, context) => {
+      if (context.user) {
+        const post = await Post.findOneAndDelete({
+          _id,
+          user: context.user._id
+        });
+        return post;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    createComment: async (parent, { postId, content }, context) => {
+      if (context.user) {
+        const comment = await Comment.create({
+          content,
+          user: context.user._id,
+          post: postId
+        });
+        return comment;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    deleteComment: async (parent, { _id }, context) => {
+      if (context.user) {
+        const comment = await Comment.findOneAndDelete({
+          _id,
+          user: context.user._id
+        });
+        return comment;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  }
 };
 
 module.exports = resolvers;
