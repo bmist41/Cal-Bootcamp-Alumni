17df00e3f38628b18e3aeb56885f241c249ba7d0
Hunter Thompson <hunterthompson025@gmail.com>
2024-09-05 21:04:40 -0700

    This commit adds the auth.js file to be used for users signing up and logging in and posts and comments.

---
+++ server/utils/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'yourSecretKey';
+const expiration = '2h';
+
+module.exports = {
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+  authMiddleware: function ({ req }) {
+    let token = req.headers.authorization;
+
+    if (token) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret);
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  }
+};
