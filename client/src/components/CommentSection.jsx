6f3e57091b385a43b30c375e095f0a33dbc3ccae
Travis McDermott <tjmcd2010@gmail.com>
2024-09-09 21:24:29 -0700

    Updates the client utils and components

---
+++ client/src/components/CommentSection.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { CREATE_COMMENT } from '../graphql/mutations';
+
+const CommentSection = ({ postId, comments }) => {
+  const [content, setContent] = useState('');
+  const [createComment] = useMutation(CREATE_COMMENT);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await createComment({ variables: { postId, content } });
+      setContent('');
+      window.location.reload();  // Reload the page to show new comments
+    } catch (error) {
+      console.error('Error creating comment:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h3>Comments</h3>
+      {comments.map((comment) => (
+        <div key={comment._id}>
+          <p>{comment.content}</p>
+          <p>by {comment.user.username} on {new Date(comment.createdAt).toLocaleString()}</p>
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <textarea
+          placeholder="Add a comment"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          required
+        ></textarea>
+        <button type="submit">Submit Comment</button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentSection;
