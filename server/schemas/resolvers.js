e0b88dcd417452cfa7a81a3308ad4cd3182787fd
bradragz <ragonesebradley@gmail.com>
2024-09-09 20:38:41 -0700

    resolvers/auth/server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,105 +1,52 @@
-const { AuthenticationError } = require('apollo-server-express');
 const { User, Post, Comment } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    posts: async () => {
-      return await Post.find().populate('user').populate('comments');
-    },
-    post: async (parent, { _id }) => {
-      return await Post.findById(_id).populate('user').populate({
-        path: 'comments',
-        populate: 'user'
-      });
-    },
-    user: async (parent, { _id }) => {
-      return await User.findById(_id);
-    },
     me: async (parent, args, context) => {
       if (context.user) {
-        return await User.findById(context.user._id);
+        return User.findById(context.user._id).populate('posts');
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError('Not logged in');
+    },
+    posts: async () => {
+      return Post.find().populate('user comments');
+    },
+    post: async (parent, { id }) => {
+      return Post.findById(id).populate('user comments');
     },
   },
-
   Mutation: {
-    login: async (parent, { username, password }) => {
-      const user = await User.findOne({ username });
-
-      if (!user || !(await user.isCorrectPassword(password))) {
-        throw new AuthenticationError('Incorrect credentials');
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) throw new AuthenticationError('User not found');
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) throw new AuthenticationError('Incorrect password');
+      const token = signToken(user);
+      return { token, user };
+    },
+    createProfile: async (parent, { linkedin, github, bio, bootcampClass, firstJobPath }, context) => {
+      if (context.user) {
+        return User.findByIdAndUpdate(context.user._id, {
+          profile: { linkedin, github, bio, bootcampClass, firstJobPath },
+        }, { new: true });
       }
-
-      const token = signToken(user);
-      return { token, user };
+      throw new AuthenticationError('Not logged in');
     },
-
-    signup: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-      return { token, user };
-    },
-
     createPost: async (parent, { title, content }, context) => {
       if (context.user) {
-        const post = await Post.create({
-          title,
-          content,
-          user: context.user._id
-        });
-        return post;
+        return Post.create({ title, content, user: context.user._id });
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError('Not logged in');
     },
-
-    updatePost: async (parent, { _id, title, content }, context) => {
-      if (context.user) {
-        const post = await Post.findOneAndUpdate(
-          { _id, user: context.user._id },
-          { title, content },
-          { new: true }
-        );
-        return post;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-
-    deletePost: async (parent, { _id }, context) => {
-      if (context.user) {
-        const post = await Post.findOneAndDelete({
-          _id,
-          user: context.user._id
-        });
-        return post;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-
     createComment: async (parent, { postId, content }, context) => {
       if (context.user) {
-        const comment = await Comment.create({
-          content,
-          user: context.user._id,
-          post: postId
-        });
-        return comment;
+        return Comment.create({ content, post: postId, user: context.user._id });
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError('Not logged in');
     },
-
-    deleteComment: async (parent, { _id }, context) => {
-      if (context.user) {
-        const comment = await Comment.findOneAndDelete({
-          _id,
-          user: context.user._id
-        });
-        return comment;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-  }
+  },
 };
 
 module.exports = resolvers;
